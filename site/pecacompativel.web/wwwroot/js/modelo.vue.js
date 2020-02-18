8ed27c45664244d0ced47836d6f2156797d6c1dd
Gláucio Oliveira <glaucio@goliveira.com>
2020-02-17 21:22:37 -0300

    commiting changes automatically

--- site/pecacompativel.web/wwwroot/js/modelo.vue.js
+++ site/pecacompativel.web/wwwroot/js/modelo.vue.js
@@ -10,7 +10,18 @@
     methods: {
         atualizaListaPecasAlternativas: function () {
             axios.get("https://localhost:44300/peca/ListarPecasAlternativas/" + this.id)
-                .then(response => (this.pecasAlternativas = response.data));
+                .then(response => {
+                    this.pecasAlternativas = response.data;
+                    let x = this.computed.listaPecasAlternativasFiltro();
+                });
+        },
+
+        removerPecaAlternativa: function (idPeca) {
+            axios.delete("https://localhost:44300/peca/" + idPeca)
+                .then(response => {
+                    this.pecasAlternativas = response.data;
+                    this.atualizaListaPecasAlternativas();
+                });
         }
     },
 
@@ -18,9 +29,24 @@
         var Id = IdPagina();
         this.id = Id;
         axios.get("https://localhost:44300/modelo/" + Id)
-            .then(response => (this.modelo = response.data));        
+            .then(response => (this.modelo = response.data));
 
         this.atualizaListaPecasAlternativas();
+    },
+
+    computed: {
+        listaPecasAlternativasFiltro() {
+            if (this.pecasAlternativas !== undefined) {
+                return this.pecasAlternativas.filter(x => {
+                    return (
+                        x.pecaNome.toLowerCase().indexOf(this.filtroTabela.toLowerCase()) > -1
+                    );
+                });
+            }
+            else {
+                return [];
+            }
+        }
     }
 });
 
